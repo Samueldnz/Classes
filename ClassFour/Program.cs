9072832c1959fff700ea484bb0408fc6e94c9f9b
Samuel Sampaio <samuelsd@dcc.ufrj.br>
2022-12-04 20:59:58 -0300

    Preparing the Class

---
+++ ClassFour/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ClassFour
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int resultado = soma(1, 3);
+        }
+
+        public  static int soma(int a, int b){
+            return a + b;
+        }
+    }
+}
