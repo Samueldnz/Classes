8fee45107ef7523216a00d7bbd1bab9ede1a324d
Samuel Sampaio <samuelsd@dcc.ufrj.br>
2022-12-05 10:07:39 -0300

    Part I

--- ClassFour/Program.cs
+++ ClassFour/Program.cs
@@ -6,9 +6,24 @@ namespace ClassFour
     {
         public static void Main(string[] args)
         {
-            int resultado = Arithmetic.soma(1, 2);
-            Console.WriteLine(resultado);
-            
+        //    int resultado = Soma(2, 1); 
+        //    Console.WriteLine(resultado);
+                Console.WriteLine(Soma(3625, 6434));
         }
+
+        private void Nada(){
+            Console.WriteLine("Nada");
+        }
+
+        // private int Soma(int a, int b){
+        //     int resultado = a + b;
+        //     return resultado;
+        // }
+
+        // private int Soma(int a, int b){
+        //     return a + b;
+        // }
+
+        private static int Soma(int a, int b) => a + b; //3
     }
 }
